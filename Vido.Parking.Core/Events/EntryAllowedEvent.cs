325ff5ed0cc4b2a15b29aa98eae6180a933bc8f4
Trần Khanh <doicanhden@gmail.com>
2014-03-22 13:13:49 +0700

    Update 3

---
+++ Vido.Parking.Core/Events/EntryAllowedEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace Vido.Parking.Events
+{
+  using System;
+  using System.Drawing;
+
+  public delegate void EntryAllowedEventHandler(object sender, EntryAllowedEventArgs e);
+
+  public class EntryAllowedEventArgs : EventArgs
+  {
+    #region Public Properties
+    public string PlateNumber { get; set; }
+    #endregion
+
+    #region Constructors
+    public EntryAllowedEventArgs(string plateNumber)
+    {
+      this.PlateNumber = plateNumber;
+    }
+    #endregion
+  }
+}
